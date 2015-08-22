3db70e392ea38e03f540fd4ddfd0275b33587137
Sudhir Pratap Yadav <yadav.1@iitj.ac.in>
2015-08-22 17:20:04 +0530

    Resistor color code calculator
    Just get resistance value from color code or vice versa

---
+++ Color_Coding_Resistor/Form1.Designer.cs
@@ -0,0 +1,150 @@
+﻿namespace Color_Coding_Resistor
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Form1));
+            this.cb1 = new System.Windows.Forms.ComboBox();
+            this.cb2 = new System.Windows.Forms.ComboBox();
+            this.cb3 = new System.Windows.Forms.ComboBox();
+            this.tf = new System.Windows.Forms.TextBox();
+            this.chb = new System.Windows.Forms.CheckBox();
+            this.SuspendLayout();
+            // 
+            // cb1
+            // 
+            this.cb1.DropDownStyle = System.Windows.Forms.ComboBoxStyle.DropDownList;
+            this.cb1.FormattingEnabled = true;
+            this.cb1.Items.AddRange(new object[] {
+            "Black",
+            "Brown",
+            "Red",
+            "Orange",
+            "Yellow",
+            "Green",
+            "Blue",
+            "Violet",
+            "Gray",
+            "White"});
+            this.cb1.Location = new System.Drawing.Point(12, 12);
+            this.cb1.Name = "cb1";
+            this.cb1.Size = new System.Drawing.Size(70, 21);
+            this.cb1.TabIndex = 0;
+            this.cb1.SelectedIndexChanged += new System.EventHandler(this.cb_SelectedIndexChanged);
+            // 
+            // cb2
+            // 
+            this.cb2.DropDownStyle = System.Windows.Forms.ComboBoxStyle.DropDownList;
+            this.cb2.FormattingEnabled = true;
+            this.cb2.Items.AddRange(new object[] {
+            "Black",
+            "Brown",
+            "Red",
+            "Orange",
+            "Yellow",
+            "Green",
+            "Blue",
+            "Violet",
+            "Gray",
+            "White"});
+            this.cb2.Location = new System.Drawing.Point(88, 12);
+            this.cb2.Name = "cb2";
+            this.cb2.Size = new System.Drawing.Size(70, 21);
+            this.cb2.TabIndex = 1;
+            this.cb2.SelectedIndexChanged += new System.EventHandler(this.cb_SelectedIndexChanged);
+            // 
+            // cb3
+            // 
+            this.cb3.DropDownStyle = System.Windows.Forms.ComboBoxStyle.DropDownList;
+            this.cb3.FormattingEnabled = true;
+            this.cb3.Items.AddRange(new object[] {
+            "Black",
+            "Brown",
+            "Red",
+            "Orange",
+            "Yellow",
+            "Green",
+            "Blue",
+            "Violet",
+            "Gray",
+            "White"});
+            this.cb3.Location = new System.Drawing.Point(164, 12);
+            this.cb3.Name = "cb3";
+            this.cb3.Size = new System.Drawing.Size(70, 21);
+            this.cb3.TabIndex = 2;
+            this.cb3.SelectedIndexChanged += new System.EventHandler(this.cb_SelectedIndexChanged);
+            // 
+            // tf
+            // 
+            this.tf.Location = new System.Drawing.Point(12, 39);
+            this.tf.Name = "tf";
+            this.tf.Size = new System.Drawing.Size(222, 20);
+            this.tf.TabIndex = 5;
+            this.tf.TextChanged += new System.EventHandler(this.tf_TextChanged);
+            // 
+            // chb
+            // 
+            this.chb.AutoSize = true;
+            this.chb.Location = new System.Drawing.Point(14, 69);
+            this.chb.Name = "chb";
+            this.chb.Size = new System.Drawing.Size(96, 17);
+            this.chb.TabIndex = 6;
+            this.chb.Text = "Always on Top";
+            this.chb.UseVisualStyleBackColor = true;
+            this.chb.CheckedChanged += new System.EventHandler(this.chb_CheckedChanged);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(240, 93);
+            this.Controls.Add(this.chb);
+            this.Controls.Add(this.tf);
+            this.Controls.Add(this.cb3);
+            this.Controls.Add(this.cb2);
+            this.Controls.Add(this.cb1);
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.FixedSingle;
+            this.Icon = ((System.Drawing.Icon)(resources.GetObject("$this.Icon")));
+            this.Name = "Form1";
+            this.ShowInTaskbar = false;
+            this.Text = "Resistor Code";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.ComboBox cb1;
+        private System.Windows.Forms.ComboBox cb2;
+        private System.Windows.Forms.ComboBox cb3;
+        private System.Windows.Forms.TextBox tf;
+        private System.Windows.Forms.CheckBox chb;
+    }
+}
+
---
+++ Color_Coding_Resistor/Form1.cs
@@ -0,0 +1,167 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Windows.Forms;
+
+namespace Color_Coding_Resistor
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+            cb1.SelectedIndex = 0;
+            cb2.SelectedIndex = 0;
+            cb3.SelectedIndex = 0;
+            tf.Text = "0";
+            this.MaximizeBox = false;
+            this.MinimizeBox = false;
+        }
+
+        private void cb_SelectedIndexChanged(object sender, EventArgs e)
+        {
+            if (tf.Focused == false)
+            {
+                int c1 = cb1.SelectedIndex;
+                int c2 = cb2.SelectedIndex;
+                int c3 = cb3.SelectedIndex;
+                double val = c1*10 + c2;
+                switch (c3)
+                {
+                    case 0: val *= 1;
+                        break;
+                    case 1: val *= 10;
+                        break;
+                    case 2: val *= 100;
+                        break;
+                    case 3: val *= 1000;
+                        break;
+                    case 4: val *= 10000;
+                        break;
+                    case 5: val *= 100000;
+                        break;
+                    case 6: val *= 1000000;
+                        break;
+                    case 7: val *= 10000000;
+                        break;
+                    case 8: val *= 100000000;
+                        break;
+                    case 9: val *= 1000000000;
+                        break;
+                    default: val = 0;
+                        break;
+                }
+                String text = "";
+                if (val >= 1000000)
+                {
+                    val = val / 1000000;
+                    text = val.ToString() + " M";
+                }
+                else if (val >= 1000)
+                {
+                    val = val / 1000;
+                    text = val.ToString() + " K";
+                }
+                else
+                {
+
+                    text = val.ToString();
+                }
+                tf.Text = text;
+            }
+        }
+
+        private void tf_TextChanged(object sender, EventArgs e)
+        {
+            if (tf.Focused == true)
+            {
+                String txt = tf.Text.Trim();
+                if (txt.Length > 0)
+                {
+                    double val = 0;
+                    String ch = (txt.Substring(txt.Length - 1)).ToLower();
+                    double mul = 1;
+                    if (ch == "k")
+                    {
+                        txt = tf.Text.Trim();
+                        txt = (txt.Substring(0, txt.Length - 1)).Trim();
+                        mul = 1000;
+
+                    }
+                    else if (ch == "m")
+                    {
+                        txt = tf.Text.Trim();
+                        txt = (txt.Substring(0, txt.Length - 1)).Trim();
+                        mul = 1000000;
+                    }
+                    if (Double.TryParse(txt, out val))
+                    {
+                        val = val * mul;
+                        if (val < 0)
+                        {
+                            MessageBox.Show("ERROR_1 : negative value", "ERROR", MessageBoxButtons.OK, MessageBoxIcon.Error);
+                            cb1.SelectedIndex = 0;
+                            cb2.SelectedIndex = 0;
+                            cb3.SelectedIndex = 0;
+                            tf.Text = "";
+                        }
+                        else if (val > 99000000000)
+                        {
+                            MessageBox.Show("ERROR_2 : max value exceeded", "ERROR", MessageBoxButtons.OK, MessageBoxIcon.Error);
+                            cb1.SelectedIndex = 0;
+                            cb2.SelectedIndex = 0;
+                            cb3.SelectedIndex = 0;
+                            tf.Text = "";
+                        }
+                        else
+                        {
+                            long r = Convert.ToInt64(val);
+                            txt = r.ToString();
+                            int l = txt.Length;
+                            if (l==1)
+                            {
+                                cb1.SelectedIndex = txt[0]-48;
+                                cb2.SelectedIndex = 0;
+                                cb3.SelectedIndex = 0;
+                            }
+                            else if (l==2)
+                            {
+                                cb1.SelectedIndex = txt[0] - 48;
+                                cb2.SelectedIndex = txt[1] - 48;
+                                cb3.SelectedIndex = 0;
+                            }
+                            else
+                            {
+                                cb1.SelectedIndex = txt[0] - 48;
+                                cb2.SelectedIndex = txt[1] - 48;
+                                cb3.SelectedIndex = l-2;
+                            }
+                        }
+                    }
+                    else
+                    {
+                        MessageBox.Show("ERROR_2 : wrong fromat","ERROR",MessageBoxButtons.OK,MessageBoxIcon.Error);
+                        cb1.SelectedIndex=0;
+                        cb2.SelectedIndex=0;
+                        cb3.SelectedIndex=0;
+                        tf.Text = "";
+                    }
+                }
+                else {
+                    cb1.SelectedIndex=0;
+                    cb2.SelectedIndex=0;
+                    cb3.SelectedIndex=0;
+                }
+            }
+        }
+
+        private void chb_CheckedChanged(object sender, EventArgs e)
+        {
+            this.TopMost = chb.Checked;
+        }
+    }
+}
